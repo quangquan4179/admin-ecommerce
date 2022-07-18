6db6af5c314ea08e596524e456f7e971c95a1df6
quangquan4179 <quangquan4179@gmail.com>
2022-07-18 16:37:05 +0700

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,7 +18,7 @@ const App = (props) => {
     <CacheProvider value={emotionCache}>
       <Head>
         <title>
-          Material Kit Pro
+          Admin
         </title>
         <meta
           name="viewport"
